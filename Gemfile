0482c761dc941a9b450c349b9da1d04cda46ed2c
mixelpixel <mixelpix@mac.com>
2017-02-07 14:04:02 -0500

    ckeditor gem install generate migrate load and set-up

--- Gemfile
+++ Gemfile
@@ -49,3 +49,6 @@ gem 'thor', '0.19.1'
 gem 'simple_form', '~>3.2'
 
 gem 'bootstrap-sass', '~> 3.3'
+
+gem 'ckeditor', '~> 4.1'
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206205806) do
+ActiveRecord::Schema.define(version: 20170207135943) do
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "ckeditor_assets", ["type"], name: "index_ckeditor_assets_on_type"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
