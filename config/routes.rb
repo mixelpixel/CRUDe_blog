d960fdcab40ae65f69dd8522ac1d9768d3c0af52
mixelpixel <mixelpix@mac.com>
2017-02-07 13:51:05 -0500

    post creation route

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root to: "posts#index"
+
+  resources :posts
 end
