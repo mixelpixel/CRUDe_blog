7f6938a700f5f3818341f36663877dcb26f3e549
mixelpixel <mixelpix@mac.com>
2017-05-18 16:47:36 -0400

    validation verbatim tutorial

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-  #This validates presence of title, and makes sure that the length is not more than 140 words
+  # This validates presence of title, and makes sure that
+  # the length is not more than 140 words
   validates :title, presence: true, length: {maximum: 140}
-  #This validates presence of body
+  # This validates presence of body
   validates :body, presence: true
 end
