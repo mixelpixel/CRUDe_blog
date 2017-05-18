b61c0165986d8c18b835c190d3e3ecf813d8b46a
mixelpixel <mixelpix@mac.com>
2017-05-18 17:35:17 -0400

    mount AvatarUploader

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #mount avatar uploader
+  mount_uploader :avatar, AvatarUploader
+  
 end
